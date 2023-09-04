c6084d340edbd443fbd6177789eb1fe7ce2ec46e
Damian Buskens <damian.buskens2003@gmail.com>
2023-09-04 14:55:10 +0200

    Update about page and add home page
    This commit wil add some minor changes to the about page and will
    add the same nav bar styling to the index page. It will also add
    a banner and buttons for the different series I will show.

---
+++ index.js
@@ -0,0 +1,10 @@
+const navBar = document.querySelector(".nav");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 1) {
+        navBar.style.backgroundColor = 'black';
+    }
+    else {
+        navBar.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+    }
+})
