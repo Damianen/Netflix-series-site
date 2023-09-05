a22c9d650b770fee28f101bc26bd169ce8f12674
Damian Buskens <113175544+Damianen@users.noreply.github.com>
2023-09-05 19:21:49 +0200

    Add changing background for header

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ window.addEventListener("scroll", () => {
     else {
         navBar.style.backgroundColor = 'rgba(0, 0, 0, 0)';
     }
-})
+});
+
+const header = document.querySelector(".header");
+const filmBarBtns = document.querySelectorAll(".Film-nav-bar li");
+
+filmBarBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        header.style.backgroundImage = `url(Assets/${btn.className}.jpg)`;
+        filmBarBtns.forEach(btn1 => {
+                btn1.classList.remove('active');
+        });
+        btn.classList.add('active');
+    });
+});
